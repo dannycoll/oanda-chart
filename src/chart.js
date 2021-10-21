e6543821d4f0974dd8f014162c54f64384cb6013
Daniel Collins <dptc98@gmail.com>
2021-10-21 18:36:54 +0100

    changing tooltip to include num. of open trades

--- src/chart.js
+++ src/chart.js
@@ -9,6 +9,21 @@ import {
 } from "recharts";
 import { useState, useEffect } from "react";
 
+const CustomTooltip = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip">
+        <p className="label">{payload[0].payload.time}</p>
+        <p className="balance-label">{`balance : ${payload[0].payload.balance}`}</p>
+        <p className="label">{`Open Trades : ${payload[0].payload.openTrades}`}</p>
+        {console.log(payload[0].payload)}
+      </div>
+    );
+  }
+
+  return null;
+};
+
 const Chart = () => {
   const [data, setData] = useState([]);
   const [percentDiff, setPercentDiff] = useState(0.0);
@@ -27,12 +42,14 @@ const Chart = () => {
     const prices = response.map((x) => ({
       balance: x[0].toFixed(2),
       time: new Date(x[1] * 1000).toLocaleTimeString("en-UK"),
+      openTrades: x[2],
     }));
     console.log(prices);
     setData(prices);
     setPercentDiff(
       (
-        (prices[prices.length - 1].balance / prices[0].balance - 1) *
+        ((prices[prices.length - 1].balance - prices[0].balance) /
+          prices[0].balance) *
         100
       ).toFixed(2)
     );
@@ -45,7 +62,10 @@ const Chart = () => {
     <div className="chart">
       {data.length > 0 && (
         <ResponsiveContainer width="95%" height="60%" className="chart">
-          <AreaChart data={data} margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
+          <AreaChart
+            data={data}
+            margin={{ top: 20, right: 30, left: 0, bottom: 0 }}
+          >
             <defs>
               <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
                 <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
@@ -54,15 +74,15 @@ const Chart = () => {
             </defs>
             <XAxis
               dataKey="time"
-              tick={{ stroke: "white", strokeWidth: 0.5 }}
+              tick={{ fill: "white", strokeWidth: 0.5 }}
             />
             <YAxis
               type="number"
               domain={["dataMin-10", "dataMax+10"]}
-              tick={{ stroke: "white", strokeWidth: 0.5 }}
+              tick={{ fill: "white", strokeWidth: 0.5 }}
             />
             <CartesianGrid strokeDasharray="3 3" />
-            <Tooltip />
+            <Tooltip content={<CustomTooltip />} />
             <Area
               type="basis"
               dataKey="balance"
