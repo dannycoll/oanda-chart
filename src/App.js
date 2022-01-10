38fc6b5cab08f782ec0af67e5f85c5f09f287a03
Daniel Collins <dptc98@gmail.com>
2022-01-10 10:33:52 +0000

    New ads

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
-import Chart from './components/chart';
+import "./App.css";
+import Chart from "./components/chart";
+import AdSense from "react-adsense";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Chart />
       </header>
+      <AdSense.Google client="ca-pub-6001071944606362" slot="1" />
     </div>
   );
 }
