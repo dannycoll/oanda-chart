e8df15707c74a13551d75fbe3882e7a46455af0a
Daniel Collins <dptc98@gmail.com>
2022-06-21 18:42:43 +0100

    Loading on position list

--- src/components/positionList.jsx
+++ src/components/positionList.jsx
@@ -4,7 +4,9 @@ import styles from './../styles/positionList.module.css';
 const PositionList = () => {
   const [positions, setPositions] = useState([]);
   const [showMarginWarning, setShowMarginWarning] = useState(false);
+  const [loading, setLoading] = useState(false);
   const fetchData = async () => {
+    setLoading(true);
     let response = await (
       await fetch(`https://api-fxtrade.oanda.com/v3/accounts/${process.env.REACT_APP_ACCOUNT_ID}/positions`, {
         mode: 'cors',
@@ -34,48 +36,63 @@ const PositionList = () => {
     });
     setPositions([...response]);
     setShowMarginWarning(parseFloat(accountSummary.marginUsed) > 0.9 * parseFloat(accountSummary.marginCloseoutNAV));
+    setLoading(false);
   };
   useEffect(() => {
     fetchData();
   }, []);
+
   const getColor = input => {
     if (input >= 0 || input === 'long') return '#82ca9d';
     return '#ff6666';
   };
+
+  const getDirection = input => {
+    if (input === 'long') return 'Long';
+    if (input === 'short') return 'Short';
+    return '-';
+  };
+
   return (
     <div className={styles.container}>
-      {showMarginWarning && <div className={styles.marginText}>High Margin!</div>}
-      <table>
-        <thead>
-          <tr>
-            <th>Pair</th>
-            <th>Profit</th>
-            <th>Direction</th>
-            <th>Trades</th>
-          </tr>
-        </thead>
-        <tbody>
-          {positions.filter(x => x.direction !== '-').length > 0 ? (
-            positions
-              .filter(x => x.direction !== '-')
-              .map(x => (
-                <tr key={x.instrument}>
-                  <td>{x.instrument.replace('_', '/')}</td>
-                  <td style={{ color: getColor(x.pl) }}>{x.pl}</td>
-                  <td style={{ color: getColor(x.direction) }}>{x.direction}</td>
-                  <td>{x.units / 1000}</td>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          {showMarginWarning && <div className={styles.marginText}>High Margin!</div>}
+          <table>
+            <thead>
+              <tr>
+                <th>Pair</th>
+                <th>Profit</th>
+                <th>Direction</th>
+                <th>Trades</th>
+              </tr>
+            </thead>
+            <tbody>
+              {positions.filter(x => x.direction !== '-').length > 0 ? (
+                positions
+                  .filter(x => x.direction !== '-')
+                  .map(x => (
+                    <tr key={x.instrument}>
+                      <td>{x.instrument.replace('_', '/')}</td>
+                      <td style={{ color: getColor(x.pl) }}>{x.pl}</td>
+                      <td style={{ color: getColor(x.direction) }}>{x.direction}</td>
+                      <td>{x.units / 1000}</td>
+                    </tr>
+                  ))
+              ) : (
+                <tr>
+                  <td></td>
+                  <td>No</td>
+                  <td>open</td>
+                  <td>positions</td>
                 </tr>
-              ))
-          ) : (
-            <tr>
-              <td></td>
-              <td>No</td>
-              <td>open</td>
-              <td>positions</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+              )}
+            </tbody>
+          </table>
+        </>
+      )}
     </div>
   );
 };
